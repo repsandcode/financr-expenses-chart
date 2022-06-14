e0dd3c1880e31b622fb33120af181772e4065085
Albert Jonathan <borjajonathann@gmail.com>
2022-06-14 17:05:16 +0800

    only hover is needed

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,12 @@
-const bar = document.querySelector('.bar');
+const bar = document.querySelectorAll('.bar');
+const amount = document.querySelectorAll('.amount');
 
-Array.from(bar).forEach((element)=>{
-  element.addEventListener('mouseover', showAmount)
+
+Array.from(bar).forEach((divBar)=>{
+  divBar.addEventListener('mouseover', showAmount)
 })
 
+
 function showAmount() {
-  
+  this.classlist.('')
 }
